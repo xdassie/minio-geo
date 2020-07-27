c1a847a9846e9ff248d6eee0cf484f1b83c138a8
xdassie <50299712+xdassie@users.noreply.github.com>
2020-07-27 14:59:31 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ MINIO_ACCESS_KEY=`cat /var/run/secrets/minio/ACCESS_KEY`
 MINIO_SECRET_KEY=`cat /var/run/secrets/minio/SECRET_KEY`
 export MINIO_ACCESS_KEY
 export MINIO_SECRET_KEY
-/minio server SERVER_LIST 2>&1 &
+/minio server $SERVER_LIST 2>&1 &
 sleep 60
 while true; do /mc config host add local http://localhost:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY ; /mc policy download local/public-downloads && mc admin info local; sleep 60; done;
